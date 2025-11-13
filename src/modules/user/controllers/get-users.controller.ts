f4cc1009d78d056c5fb20ba87c44485dfde73c55
Ahmed Wael <awael3977@gmail.com>
2025-11-13 15:55:55 +0200

    Refactor email module and user controller: update import paths for environment variables and utilities; modify user controller to return a success status and user records in the response.

--- src/modules/user/controllers/get-users.controller.ts
+++ src/modules/user/controllers/get-users.controller.ts
@@ -4,7 +4,8 @@ import { Request, Response } from "express";
 export const getUsersController = async (req: Request, res: Response) => {
   const users = await db.collection("users").find().toArray();
 
-  return res.status(404).json({
-    message: "No users found",
+  return res.status(200).json({
+    message: "Users fetched successfully",
+    records: users,
   });
 };
