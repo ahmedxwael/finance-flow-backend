2d3750261986a63742e8a61e1d6ac3f96eb0bc07
Ahmed Wael <awael3977@gmail.com>
2025-11-13 22:32:16 +0200

    Add logging for fetched users in getUsersController

--- src/modules/user/controllers/get-users.controller.ts
+++ src/modules/user/controllers/get-users.controller.ts
@@ -4,6 +4,7 @@ import { db } from "../../../core";
 export const getUsersController = async (req: Request, res: Response) => {
   const users = await db.collection("users").find().toArray();
 
+  console.log("users", users);
   return res.status(200).json({
     message: "Users fetched successfully",
     records: users,
